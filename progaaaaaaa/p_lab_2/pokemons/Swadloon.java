7d247d359b3396102e56897078c904a9a568575e
MKoblents <501832@niuitmo.ru>
2025-11-29 14:37:32 +0300

    code p_lab_2 after passing

--- progaaaaaaa/p_lab_2/pokemons/Swadloon.java
+++ progaaaaaaa/p_lab_2/pokemons/Swadloon.java
@@ -5,9 +5,8 @@ import attacks.Rest;
 import attacks.Deth;
 import ru.ifmo.se.pokemon.*;
 public class Swadloon extends Sewaddle{
-    Swadloon(String name, int leavl){
-        // super(name, leavl);
-        this(name, leavl, 10);
+    public Swadloon(String name, int leavl){
+        super(name, leavl);
         super.setStats(55, 63.0, 90.0, 50.0, 80.0, 42.0);
         super.setMove(new Deth());
         System.out.println("1");
@@ -15,9 +14,12 @@ public class Swadloon extends Sewaddle{
         // this.setMove(new Deth());
 
     }
-    private Swadloon(String name, int leavl, int HP){
-        super(name, leavl);
-        System.out.println("2");
-        super.setStats(HP, 63.0, 90.0, 50.0, 80.0, 42.0);
+    public static void hello(){
+        System.out.println("HELLO");
     }
+    // private Swadloon(String name, int leavl, int HP){
+    //     super(name, leavl);
+    //     System.out.println("2");
+    //     super.setStats(HP, 63.0, 90.0, 50.0, 80.0, 42.0);
+    // }
 }
--- progaaaaaaa/p_lab_2/programm/Programm.java
+++ progaaaaaaa/p_lab_2/programm/Programm.java
@@ -3,26 +3,29 @@ package programm;
 import pokemons.Leavanny;
 import pokemons.Lycanroc;
 import pokemons.Spiritomb;
-import pokemons.Swadloon;
+// import pokemons.Swadloon;
 import ru.ifmo.se.pokemon.Battle;
 import ru.ifmo.se.pokemon.Battle;
 import ru.ifmo.se.pokemon.Pokemon;
 import pokemons.*;
 import java.util.Random;
-
+import static ru.ifmo.se.pokemon.Stat.*;
+import static ru.ifmo.se.pokemon.Stat.*;
+import static pokemons.Swadloon.*;
 public class Programm{
     public static void main(String args []){
+        hello();
         Battle b = new Battle();
         Leavanny p1 = new Leavanny("1",1);
         Lycanroc p2 = new Lycanroc("2",36);
         Rockruff p3 = new Rockruff("3", 32);
         Sewaddle p4 =new Sewaddle("4",1);
         Spiritomb p5 = new Spiritomb("5",7);
-        Swadloon p6 = new Swadloon("6",1);
+        //Swadloon p6 = new Swadloon("6",1);
         b.addAlly(p1);
         b.addAlly(p4);
         b.addAlly(p3);
-        b.addFoe(p6);
+        //b.addFoe(p6);
         b.addFoe(p5);
         b.addFoe(p2);
         b.go();
